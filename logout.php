57f7dbea1a628a57ab036d4545d1982e5d4e5a06
britneyangellyrodriguezdiaz-eng <britneyangellyrodriguezdiaz@gmail.com>
2026-07-08 08:30:07 -0600

    Remove commented lines from logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-// Destruir todas las variables de sesión
+
 $_SESSION = array();
-// Destruir la sesión física
+
 session_destroy();
-// Redirigir al login
+
 header("Location: login.php");
 exit();
 ?>
